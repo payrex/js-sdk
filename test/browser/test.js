e3e46f645d7fafbd2f5cea718d1a72420a157631
payrex <payrex@no-email.com>
2018-02-07 14:39:32 +0200

    Added more tests

--- test/browser/test.js
+++ test/browser/test.js
@@ -1,13 +1,26 @@
 const PayrexSdk = window.PayrexSdk;
 
 describe('PayrexSdk', () => {
-  it('should have interface', () => {
+  it('should create successful with options', () => {
     assert.strictEqual(typeof PayrexSdk, 'function')
-    const sdk = new PayrexSdk();
-    assert.strictEqual(typeof sdk, 'object')
-    assert.strictEqual(typeof sdk.get, 'function')
-    assert.strictEqual(typeof sdk.post, 'function')
-    assert.strictEqual(typeof sdk.put, 'function')
-    assert.strictEqual(typeof sdk.remove, 'function')
+    const sdk = new PayrexSdk({ baseUrl: 'http://localhost/' });
+    assert.strictEqual(typeof sdk, 'object');
+    assert.strictEqual(sdk.publicKey, '');
+    assert.strictEqual(sdk.secretKey, '');
+    assert.strictEqual(sdk.baseUrl, 'http://localhost/');
+    assert.strictEqual(typeof sdk.get, 'function');
+    assert.strictEqual(typeof sdk.post, 'function');
+    assert.strictEqual(typeof sdk.put, 'function');
+    assert.strictEqual(typeof sdk.remove, 'function');
+  });
+  it('should not throw errors', () => {
+    new PayrexSdk();
+    new PayrexSdk({ publicKey: '' });
+    new PayrexSdk({ publicKey: 'AAA' });
+    new PayrexSdk({ secretKey: '' });
+    new PayrexSdk({ secretKey: 'AAA' });
+    new PayrexSdk({ baseUrl: 'http://localhost' });
+    new PayrexSdk({ baseUrl: 'http://localhost/' });
+    new PayrexSdk({ baseUrl: 'https://localhost/' });
   });
 });
